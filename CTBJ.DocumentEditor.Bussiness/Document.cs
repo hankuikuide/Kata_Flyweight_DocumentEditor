d75986751da62f351725563491a08c41dd0405aa
hankuikuide <hankuikuide@163.com>
2013-09-26 12:40:55 +0800

    把Position类从Document类中拿出来,放在单独的类文件.

--- CTBJ.DocumentEditor.Bussiness/Document.cs
+++ CTBJ.DocumentEditor.Bussiness/Document.cs
@@ -41,18 +41,4 @@ namespace CTBJ.DocumentEditor.Bussiness
             return context[position];
         }
     }
-
-    public class Position
-    {
-        private int x;
-
-        private int y;
-
-        public Position(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-
-        }
-    }
 }
---
+++ CTBJ.DocumentEditor.Bussiness/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTBJ.DocumentEditor.Bussiness
+{
+    public class Position
+    {
+        private int x;
+
+        private int y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+    }
+}
